21f8b4dafd48efa4d76b51e3ab257414703687b3
Florian <florian@poissonmail.fr>
2024-10-17 11:02:53 +0200

    add styles et assets

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,21 @@
+import '../styles/Cart.css'
+
 function Cart() {
     const monsteraPrice = 8
     const lierrePrice = 10
     const bouquetPrice = 15
 
-    return (<div>
-        <h2>Panier</h2>
-        <ul>
-            <li>Monstera : {monsteraPrice}</li>
-            <li>Lierre : {lierrePrice}</li>
-            <li>Bouquet de fleurs : {bouquetPrice}</li>
-        </ul>
-        Total : {monsteraPrice + lierrePrice + bouquetPrice}€
-    </div>)
+    return (
+        <div className="lmj-cart">
+            <h2>Panier</h2>
+            <ul>
+                <li>Monstera : {monsteraPrice}</li>
+                <li>Lierre : {lierrePrice}</li>
+                <li>Bouquet de fleurs : {bouquetPrice}</li>
+            </ul>
+            Total : {monsteraPrice + lierrePrice + bouquetPrice}€
+        </div>
+    )
 }
 
 
